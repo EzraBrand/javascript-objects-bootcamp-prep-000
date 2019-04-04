5671244f2781937cc28f07e55d6f17099dfe4462
Ezra Brand <ezrabrand@gmail.com>
2019-04-04 19:58:33 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -55,3 +55,9 @@ function updatePlaylist (playlist, artistName, songTitle) {
 
   return playlist
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName
+
+  return playlist
+}
